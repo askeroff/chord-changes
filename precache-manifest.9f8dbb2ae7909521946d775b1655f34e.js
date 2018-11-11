a22c8ca18ad2a57ae67d1df2b5297ec799a2c9d8
Javid Askerov <askerovlab@gmail.com>
2018-11-11 14:35:53 +0400

    Updates

---
+++ precache-manifest.9f8dbb2ae7909521946d775b1655f34e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "121a089f779b9cd33621",
+    "url": "/chord-changes/static/js/runtime~main.121a089f.js"
+  },
+  {
+    "revision": "398fe4b47ccc445f26b6",
+    "url": "/chord-changes/static/js/main.398fe4b4.chunk.js"
+  },
+  {
+    "revision": "de54985b989a9342265a",
+    "url": "/chord-changes/static/js/1.de54985b.chunk.js"
+  },
+  {
+    "revision": "398fe4b47ccc445f26b6",
+    "url": "/chord-changes/static/css/main.8ab34cc0.chunk.css"
+  },
+  {
+    "revision": "dc95a7d3b956c5126d741cff8ef3db5c",
+    "url": "/chord-changes/index.html"
+  }
+];
